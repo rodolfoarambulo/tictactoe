8a5341700ee44e3c851b81c9f14d5f5afaa8d056
rodolfoarambulo <108365768+rodolfoarambulo@users.noreply.github.com>
2023-09-07 17:45:57 +0800

    Add functions for marking gameboard spots

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 'use strict';
 
 const gameBoard = (() => {
-  let _gameState = ['X', 'O', 'X', 'X', 'O', 'O', 'X', 'O', ''];
+  let _gameState = ['', '', '', '', '', '', '', '', ''];
+  let _currentPlayer = {};
+  let _otherPlayer = {};
+
+  const initializeGame = (player1, player2) => {
+    setCurrentPlayer(player1);
+    setOtherPlayer(player2);
+    displayController.displayGameBoard();
+  }
 
   const getGameState = () => {
     return _gameState;
@@ -11,17 +19,84 @@ const gameBoard = (() => {
     return _gameState[index];
   }
 
-  const changeGameState = (index, marker) => {
-    _gameState[index] = marker;
+  const changeGameState = (index) => {
+    _gameState[index] = _currentPlayer.getMarker();
   }
 
   const resetGameState = () => {
     for (let i = 0; i < _gameState.length; i++) {
       _gameState[i] = '';
     }
+    setCurrentPlayer({});
   }
 
-  return { getGameState, getGameStateByIndex, changeGameState, resetGameState };
+  const getCurrentPlayer = () => {
+    return _currentPlayer;
+  }
+
+  const getOtherPlayer = () => {
+    return _otherPlayer;
+  }
+
+  const setCurrentPlayer = (player) => {
+    _currentPlayer = player;
+  }
+
+  const setOtherPlayer = (player) => {
+    _otherPlayer = player;
+  }
+
+  const swapCurrentPlayer = () => {
+    let swap = getCurrentPlayer();
+    setCurrentPlayer(getOtherPlayer());
+    setOtherPlayer(swap);
+  }
+
+  return {
+    initializeGame,
+
+    getGameState,
+    getGameStateByIndex,
+    changeGameState,
+    resetGameState,
+
+    getCurrentPlayer,
+    getOtherPlayer,
+    setCurrentPlayer,
+    setOtherPlayer,
+    swapCurrentPlayer
+  };
+
+})();
+
+const displayController = (() => {
+  const _gameDisplay = document.querySelector('.gameDisplay');
+
+  const _generateButton = (index) => {
+    let newButton = document.createElement('button');
+    newButton.value = index;
+    newButton.textContent = gameBoard.getGameStateByIndex(index);
+    newButton.addEventListener('click', () =>{
+      if(gameBoard.getGameStateByIndex(newButton.value) === '') {
+        gameBoard.changeGameState(newButton.value);
+        gameBoard.swapCurrentPlayer();
+      }
+      displayController.displayGameBoard();
+    });
+    return newButton;
+  }
+
+  const displayGameBoard = () => {
+    while (_gameDisplay.firstChild) {
+      _gameDisplay.removeChild(_gameDisplay.firstChild);
+    }
+
+    for (let i = 0; i < 9; i++) {
+      _gameDisplay.appendChild(_generateButton(i));
+    }
+  }
+
+  return { displayGameBoard };
 
 })();
 
@@ -31,25 +106,7 @@ const playerFactory = (name, marker) => {
   return { getName, getMarker };
 }
 
-const displayController = (() => {
-  const _gameDisplay = document.querySelector('.gameDisplay');
-
-  const _generateButton = (index) => {
-    let newButton = document.createElement('button');
-    newButton.textContent = gameBoard.getGameStateByIndex(index);
-    return newButton;
-  }
-
-  const displayGameBoard = () => {
-    while(_gameDisplay.firstChild) {
-      _gameDisplay.removeChild(_gameDisplay.firstChild);
-    }
-    
-    for (let i = 0; i < 9; i++) {
-      _gameDisplay.appendChild(_generateButton(i));
-    }
-  }
-
-  return { displayGameBoard };
-
-})();
+// testing purposes
+let player1 = playerFactory('Chewy', 'X');
+let player2 = playerFactory('Hayase', 'O');
+gameBoard.initializeGame(player1, player2);
